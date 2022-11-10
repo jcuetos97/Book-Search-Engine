2472b3d4fa4afbc8fafe7372a26e2cd5617fa0a5
unknown <jcuetos97@gmail.com>
2022-11-10 11:34:06 -0600

    Update server.js
    Created a new instance of Apollo server with GraphQL schema

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,13 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { Book, User } = require('./models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+
+    },
+
+    Mutation: {
+        
+    }
+}
